fe5b261a11c2c1414f69246b8ea910deae41bf3b
JulieBodette <jr3python@gmail.com>
2023-05-31 11:23:35 -0400

    Initial commit

---
+++ Page1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DataGridViewProgressBar
+{
+	partial class Page1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Wisej Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new Wisej.Web.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.Location = new System.Drawing.Point(366, 93);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(417, 221);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// Page1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+			this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Page1";
+			this.Size = new System.Drawing.Size(983, 430);
+			this.Load += new System.EventHandler(this.Page1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Wisej.Web.DataGridView dataGridView1;
+	}
+}
+
---
+++ Page1.cs
@@ -0,0 +1,26 @@
+﻿
+using Wisej.Web;
+
+namespace DataGridViewProgressBar
+{
+	public partial class Page1 : Page
+	{
+		public Page1()
+		{
+			InitializeComponent();
+		}
+
+		private void Page1_Load(object sender, System.EventArgs e)
+		{
+			dataGridView1.RowCount = 2;
+			dataGridView1.ColumnCount = 2;
+
+			var progressBar = new ProgressBar();
+			progressBar.Minimum = 0;
+			progressBar.Maximum = 100;
+			progressBar.Value = 50;
+			dataGridView1.Rows[0].Cells[0].Control = progressBar;
+		}
+
+	}
+}
